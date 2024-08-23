2e3ce28468b98436fc22500b3bab5c5c8dda7dca
Ting-Jun Wang <levi900227@gmail.com>
2024-08-23 16:10:13 +0800

    fix: db auto reconnect

--- src/cml_account.py
+++ src/cml_account.py
@@ -3,7 +3,7 @@ import os
 
 class CMLAccountManager():
     def __init__(self, db_username, db_password) -> None:
-        config = {
+        self.config = {
             'user': db_username,
             'password': db_password,
             'host': 'cml4.csie.ntu.edu.tw',  # 默認是 'localhost'
@@ -12,11 +12,20 @@ class CMLAccountManager():
             'ssl_disabled': True
         }
 
-        self.conn = mysql.connector.connect(**config)
+        self.conn = mysql.connector.connect(**self.config)
         print("Connection successful")
+        self.conn.close()
 
     def find_user(self, username):
-        cursor = self.conn.cursor()
+        try:
+            cursor = self.conn.cursor()
+        except:
+            if self.conn.is_connected():
+                self.conn.close()
+            print("Restart DB Connection")
+            self.conn = mysql.connector.connect(**self.config)
+            cursor = self.conn.cursor()
+
         query = "SELECT * FROM `People` WHERE `account`=%s"
         param = (username, )
 
